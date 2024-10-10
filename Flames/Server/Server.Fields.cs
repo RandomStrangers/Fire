cd110b0f492c7d9867e835ff485f8278396a242e
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2024-10-10 18:39:56 -0400

    Update Server.Fields.cs

--- Flames/Server/Server.Fields.cs
+++ Flames/Server/Server.Fields.cs
@@ -40,11 +40,11 @@ namespace Flames
         public static PlayerMetaList Notes = new PlayerMetaList("text/notes.txt");
 #if CORE
         /// <summary> *** DO NOT USE THIS! *** Use VersionString, as this field is a constant and is inlined if used. </summary>
-        public const string InternalVersion = "1.0.2.6";
+        public const string InternalVersion = "1.0.2.9";
         public static string SoftwareName = "&4H&6a&5r&0m&7o&2n&dy&a";
 #else
         /// <summary> *** DO NOT USE THIS! *** Use VersionString, as this field is a constant and is inlined if used. </summary>
-        public const string InternalVersion = "9.0.5.0";
+        public const string InternalVersion = "9.0.5.3";
         public static string SoftwareName = "&4F&cl&4a&cm&4e&cs";
 #endif
         public static string Version { get { return InternalVersion; } }
