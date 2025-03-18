13925e76234e63949475a36a600c6f479947a8bc
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-03-18 12:37:39 -0400

    Update Compiler.cs

--- Flames/Modules/Compiling/Compiler.cs
+++ Flames/Modules/Compiling/Compiler.cs
@@ -17,6 +17,7 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
  */
+#if !F_DOTNET
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -250,3 +251,4 @@ namespace Flames.Modules.Compiling
         }
     }
 }
+#endif
