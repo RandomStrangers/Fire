25230d6c61326a5c8239c2f57cd25ceeccfa7ffe
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-03-18 12:37:43 -0400

    Update CompilerFrontends.cs

--- Flames/Modules/Compiling/CompilerFrontends.cs
+++ Flames/Modules/Compiling/CompilerFrontends.cs
@@ -17,6 +17,7 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
  */
+#if !F_DOTNET
 using Flames.Modules.Compiling;
 using System.CodeDom.Compiler;
 
@@ -365,3 +366,4 @@ class {0} extends Plugin
         }
     }
 }
+#endif
