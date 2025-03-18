2badb2269be0d20ff6ef80f7c189cf65e397fab2
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-03-18 12:34:13 -0400

    Update CmdCompile.cs

--- Flames/Modules/Compiling/CmdCompile.cs
+++ Flames/Modules/Compiling/CmdCompile.cs
@@ -15,6 +15,7 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
  */
+#if !F_DOTNET
 using Flames.Commands;
 using Flames.Scripting;
 
@@ -98,3 +99,4 @@ namespace Flames.Modules.Compiling
         }
     }
 }
+#endif
