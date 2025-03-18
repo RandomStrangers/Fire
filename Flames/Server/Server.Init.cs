7d43e08f5ecd1b1d2040812340eaa917264bd717
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-03-18 12:52:02 -0400

    Update Server.Init.cs

--- Flames/Server/Server.Init.cs
+++ Flames/Server/Server.Init.cs
@@ -50,7 +50,7 @@ namespace Flames
             Level.LoadMetadata(mainLevel);
             LevelInfo.Add(mainLevel);
         }
-
+#if !F_DOTNET
         public static void LoadAllPlugins(SchedulerTask task)
         {
             Plugin.LoadAll();
@@ -58,6 +58,11 @@ namespace Flames
         public static void LoadAllSimplePlugins(SchedulerTask task)
         {
             Plugin_Simple.LoadAll();
+        }
+#endif
+        public static void LoadAllNewPlugins(SchedulerTask task)
+        {
+            NewPlugin.LoadAll();
         }
 
         public static void InitPlayerLists(SchedulerTask task)
