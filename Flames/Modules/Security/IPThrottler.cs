fb4fb1d6f5ab79cf56815b1b20f929850d88bbbc
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-03-18 12:59:16 -0400

    Update IPThrottler.cs

--- Flames/Modules/Security/IPThrottler.cs
+++ Flames/Modules/Security/IPThrottler.cs
@@ -26,7 +26,7 @@ using Flames.Tasks;
 
 namespace Flames.Modules.Security
 {
-    public class IPThrottler : NewPlugin
+    public class IPThrottler : Plugin
     {
         public override string name { get { return "IPThrottler"; } }
 
