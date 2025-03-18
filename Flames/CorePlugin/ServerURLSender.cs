dfbf661ddb3c4e372fe5c2fbdc051894684290f6
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-03-18 12:58:15 -0400

    Update ServerURLSender.cs

--- Flames/CorePlugin/ServerURLSender.cs
+++ Flames/CorePlugin/ServerURLSender.cs
@@ -4,7 +4,7 @@ using Flames.Tasks;
 
 namespace Flames.Core
 {
-    public class ServerURLSender : NewPlugin
+    public class ServerURLSender : Plugin
     {
         public override string name { get { return "Say URL"; } }
         public override string creator { get { return Colors.Strip(Server.SoftwareName) + " team"; } }
