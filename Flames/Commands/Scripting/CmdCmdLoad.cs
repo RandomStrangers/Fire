451611876439a0245086bd0a4ab47f0dd1ffe3b6
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-03-19 08:21:13 -0400

    Update CmdCmdLoad.cs

--- Flames/Commands/Scripting/CmdCmdLoad.cs
+++ Flames/Commands/Scripting/CmdCmdLoad.cs
@@ -15,6 +15,7 @@
     or implied. See the Licenses for the specific language governing
     permissions and limitations under the Licenses.
 */
+#if !F_DOTNET
 using Flames.Scripting;
 
 namespace Flames.Commands.Scripting
@@ -47,3 +48,4 @@ namespace Flames.Commands.Scripting
         }
     }
 }
+#endif
