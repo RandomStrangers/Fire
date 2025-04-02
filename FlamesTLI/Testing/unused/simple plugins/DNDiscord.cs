3f044f33640f1a161486e71381e08d26e7817b8b
RandomStrangers <jackstage527@gmail.com>
2025-04-02 16:52:06 -0400

    TLI update.

---
+++ FlamesTLI/Testing/unused/simple plugins/DNDiscord.cs
@@ -0,0 +1,16 @@
+﻿using Flames.Added;
+namespace Flames.Modules.Relay.DNDiscord
+{
+    public class DNDiscord : Addon
+    {
+        public override string Name { get { return "DNDiscord"; } }
+        public override void Load()
+        {
+            Logger.Log(LogType.Debug, Name + " says hi.");
+        }
+        public override void Unload()
+        {
+            Logger.Log(LogType.Debug, Name + " says goodbye.");
+        }
+    }
+}
